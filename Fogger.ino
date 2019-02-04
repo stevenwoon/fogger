f8603106ad05740d035bdf4c61824d085d480d50
Steven Woon <stevenwoon@hotmail.com>
2019-02-03 23:38:58 -0600

    Update 2.4MHz PWM

--- Fogger.ino
+++ Fogger.ino
@@ -6,12 +6,25 @@
 
 const byte pinOut = 6;
  
-void setup() {
+void setup4MHz() {
   pinMode (pinOut, OUTPUT); 
   TIMSK0 = 0;  // no interrupts
   Timer0::setMode (2, Timer0::PRESCALE_1, Timer0::TOGGLE_A_ON_COMPARE);
   OCR0A = 1;  // count to 2 (4MHz square wave)
   // OCR0A = 0;  // count to 2 (8MHz square wave)
 }  // end of setup
+
+void setup2_4MHz() {
+  pinMode(3, OUTPUT);
+  TCCR2A = 0x33;
+  TCCR2B = 0x09;
+  OCR2A = 0x06;
+  OCR2B = 0x03;
+}
+
+
+void setup() {
+  setup2_4MHz();
+}
 
 void loop() {}
